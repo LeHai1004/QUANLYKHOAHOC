3de3858d58cc2e3192528fdd05ba21b766267488
LeHai1004 <lehai191006@gmail.com>
2026-04-13 14:41:13 +0700

    Chạy Thành Công

---
+++ QuanLyKhoaHoc/Controllers/DangKiesController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLyKhoaHoc.Models;
+
+namespace QuanLyKhoaHoc.Controllers
+{
+    public class DangKiesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public DangKiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: DangKies
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.DangKys.Include(d => d.KhoaHoc).Include(d => d.SinhVien);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: DangKies/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dangKy = await _context.DangKys
+                .Include(d => d.KhoaHoc)
+                .Include(d => d.SinhVien)
+                .FirstOrDefaultAsync(m => m.MaDK == id);
+            if (dangKy == null)
+            {
+                return NotFound();
+            }
+
+            return View(dangKy);
+        }
+
+        // GET: DangKies/Create
+        public IActionResult Create()
+        {
+            ViewData["MaKH"] = new SelectList(_context.KhoaHocs, "MaKH", "TenKH");
+            ViewData["MaSV"] = new SelectList(_context.SinhViens, "MaSV", "HoTen");
+            return View();
+        }
+
+        // POST: DangKies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaDK,MaSV,MaKH")] DangKy dangKy)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dangKy);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKH"] = new SelectList(_context.KhoaHocs, "MaKH", "TenKH", dangKy.MaKH);
+            ViewData["MaSV"] = new SelectList(_context.SinhViens, "MaSV", "HoTen", dangKy.MaSV);
+            return View(dangKy);
+        }
+
+        // GET: DangKies/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dangKy = await _context.DangKys.FindAsync(id);
+            if (dangKy == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaKH"] = new SelectList(_context.KhoaHocs, "MaKH", "TenKH", dangKy.MaKH);
+            ViewData["MaSV"] = new SelectList(_context.SinhViens, "MaSV", "HoTen", dangKy.MaSV);
+            return View(dangKy);
+        }
+
+        // POST: DangKies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaDK,MaSV,MaKH")] DangKy dangKy)
+        {
+            if (id != dangKy.MaDK)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dangKy);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DangKyExists(dangKy.MaDK))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaKH"] = new SelectList(_context.KhoaHocs, "MaKH", "TenKH", dangKy.MaKH);
+            ViewData["MaSV"] = new SelectList(_context.SinhViens, "MaSV", "HoTen", dangKy.MaSV);
+            return View(dangKy);
+        }
+
+        // GET: DangKies/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dangKy = await _context.DangKys
+                .Include(d => d.KhoaHoc)
+                .Include(d => d.SinhVien)
+                .FirstOrDefaultAsync(m => m.MaDK == id);
+            if (dangKy == null)
+            {
+                return NotFound();
+            }
+
+            return View(dangKy);
+        }
+
+        // POST: DangKies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dangKy = await _context.DangKys.FindAsync(id);
+            if (dangKy != null)
+            {
+                _context.DangKys.Remove(dangKy);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DangKyExists(int id)
+        {
+            return _context.DangKys.Any(e => e.MaDK == id);
+        }
+    }
+}
---
+++ QuanLyKhoaHoc/Migrations/20260413073432_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyKhoaHoc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KhoaHocs",
+                columns: table => new
+                {
+                    MaKH = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenKH = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KhoaHocs", x => x.MaKH);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SinhViens",
+                columns: table => new
+                {
+                    MaSV = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SinhViens", x => x.MaSV);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DangKys",
+                columns: table => new
+                {
+                    MaDK = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MaSV = table.Column<int>(type: "int", nullable: false),
+                    MaKH = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DangKys", x => x.MaDK);
+                    table.ForeignKey(
+                        name: "FK_DangKys_KhoaHocs_MaKH",
+                        column: x => x.MaKH,
+                        principalTable: "KhoaHocs",
+                        principalColumn: "MaKH",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DangKys_SinhViens_MaSV",
+                        column: x => x.MaSV,
+                        principalTable: "SinhViens",
+                        principalColumn: "MaSV",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangKys_MaKH",
+                table: "DangKys",
+                column: "MaKH");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DangKys_MaSV",
+                table: "DangKys",
+                column: "MaSV");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DangKys");
+
+            migrationBuilder.DropTable(
+                name: "KhoaHocs");
+
+            migrationBuilder.DropTable(
+                name: "SinhViens");
+        }
+    }
+}
