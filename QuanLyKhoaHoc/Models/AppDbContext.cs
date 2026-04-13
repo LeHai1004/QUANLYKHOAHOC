57b918732d02e2f0ec031a741898d94589e1cb47
LeHai1004 <lehai191006@gmail.com>
2026-04-13 14:29:56 +0700

    Tạo DbContext

---
+++ QuanLyKhoaHoc/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QuanLyKhoaHoc.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<SinhVien> SinhViens { get; set; }
+        public DbSet<KhoaHoc> KhoaHocs { get; set; }
+        public DbSet<DangKy> DangKys { get; set; }
+    }
+}
