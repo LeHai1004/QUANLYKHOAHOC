9a8c03c974eef94fe9fd534db86647951ffda25e
LeHai1004 <lehai191006@gmail.com>
2026-04-13 14:49:08 +0700

    update

--- QuanLyKhoaHoc/Controllers/SinhViensController.cs
+++ QuanLyKhoaHoc/Controllers/SinhViensController.cs
@@ -18,43 +18,33 @@ namespace QuanLyKhoaHoc.Controllers
             _context = context;
         }
 
-        // GET: SinhViens
         public async Task<IActionResult> Index()
         {
             return View(await _context.SinhViens.ToListAsync());
         }
 
-        // GET: SinhViens/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var sinhVien = await _context.SinhViens
                 .FirstOrDefaultAsync(m => m.MaSV == id);
-            if (sinhVien == null)
-            {
-                return NotFound();
-            }
+            if (sinhVien == null) return NotFound();
 
             return View(sinhVien);
         }
 
-        // GET: SinhViens/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: SinhViens/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("MaSV,HoTen")] SinhVien sinhVien)
         {
+            ModelState.Remove("DangKys");
+
             if (ModelState.IsValid)
             {
                 _context.Add(sinhVien);
@@ -64,33 +54,22 @@ namespace QuanLyKhoaHoc.Controllers
             return View(sinhVien);
         }
 
-        // GET: SinhViens/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var sinhVien = await _context.SinhViens.FindAsync(id);
-            if (sinhVien == null)
-            {
-                return NotFound();
-            }
+            if (sinhVien == null) return NotFound();
             return View(sinhVien);
         }
 
-        // POST: SinhViens/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("MaSV,HoTen")] SinhVien sinhVien)
         {
-            if (id != sinhVien.MaSV)
-            {
-                return NotFound();
-            }
+            if (id != sinhVien.MaSV) return NotFound();
+
+            ModelState.Remove("DangKys");
 
             if (ModelState.IsValid)
             {
@@ -101,39 +80,25 @@ namespace QuanLyKhoaHoc.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!SinhVienExists(sinhVien.MaSV))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!SinhVienExists(sinhVien.MaSV)) return NotFound();
+                    else throw;
                 }
                 return RedirectToAction(nameof(Index));
             }
             return View(sinhVien);
         }
 
-        // GET: SinhViens/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var sinhVien = await _context.SinhViens
                 .FirstOrDefaultAsync(m => m.MaSV == id);
-            if (sinhVien == null)
-            {
-                return NotFound();
-            }
+            if (sinhVien == null) return NotFound();
 
             return View(sinhVien);
         }
 
-        // POST: SinhViens/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
